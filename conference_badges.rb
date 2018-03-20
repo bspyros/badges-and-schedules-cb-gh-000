410625ca92296775a4ded810510488790b93848d
Spyros Bartokaymenos <tp4417@edu.teicrete.gr>
2018-03-20 17:39:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,12 @@
 def badge_maker(name)
   return "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+
+  return badges
+end
